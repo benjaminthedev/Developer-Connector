84ef6cdf695a7240eb4286e7ee853708d62461d8
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-07-18 16:04:54 +0100

    project update

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
 // @route  GET api/users
-// @desc   Test route
+// @desc   Register User     
 // @access Public 
-router.get('/', (req, res) => res.send('User Route'))
+router.post('/', [
+    check('name', 'Name is needed').not().isEmpty(),
+    check('email', 'add read email').isEmail(),
+    check('password', 'add a password dummy').isLength({ min: 6 })
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    res.send('User Route')
+});
 
 module.exports = router;
